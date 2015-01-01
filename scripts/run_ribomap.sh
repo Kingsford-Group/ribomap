1ea3d54960c84d0351cd4a12af068cdd76e6411a
Hao Wang <hw1@cs.cmu.edu>
2015-01-01 01:14:51 -0500

    updated shell script to use the new executable

--- scripts/run_ribomap.sh
+++ scripts/run_ribomap.sh
@@ -3,7 +3,7 @@
 # default parameters
 #=============================
 # please fill this line out by yourself
-work_dir=/home/hw1/scratch/ribojamdetector/
+work_dir=/home/hw1/scratch/ribomap/
 # references -- required!!! please fill out these lines
 transcript_fa=/home/hw1/scratch/ribojamdetector/transcriptome/protein_coding_33_filtered.fasta
 rrna_fa=/home/hw1/scratch/ribojamdetector/transcriptome/rRNA.fa
@@ -21,9 +21,9 @@ offset=15 # P-site offset
 # preprocess
 fasta_dir=${work_dir}data/fasta/
 # star index
-star_idx_dir=${work_dir}StarIndex/
-rrna_idx=${star_idx_dir}rrna/
-transcript_idx=${star_idx_dir}cdsb/
+star_idx_dir=${work_dir}StarIndex/yeast/
+rrna_idx=${star_idx_dir}contaminant/
+transcript_idx=${star_idx_dir}transcript/
 # star outputs
 tmp_dir=${work_dir}tmp/
 # star params
@@ -32,8 +32,20 @@ SAM_params="--outSAMtype BAM Unsorted --outSAMmode NoQS --outSAMprimaryFlag AllB
 # salmon
 sm_odir=${work_dir}sm_quant
 # ribomap
-src_dir=${work_dir}src
 output_dir=${work_dir}outputs
+#=============================
+# functions
+#=============================
+# check whether file generated successfully at each single step
+# quit program if file failed to be generated
+# check_file $file_name $error_msg
+check_file ()
+{
+    if [ ! -f "$1" ]; then
+	echo "$2"
+	exit
+    fi
+}
 #=============================
 # make directories
 #=============================
@@ -52,9 +64,18 @@ fastx_pipe="fastx_clipper -Q33 -a ${adapter} -l ${min_fplen} -n -v | fastq_to_fa
 rna_fa=${fasta_dir}${rna_core}.fa
 ribo_fa=${fasta_dir}${ribo_core}.fa
 ribo_size_fa=${fasta_dir}${ribo_core}-size.fa
-# zcat ${rnaseq_fq} | ${fastx_pipe} -o ${rna_fa}
-# zcat ${riboseq_fq} | ${fastx_pipe} | fastx_collapser -v -o ${ribo_fa}
-# python filter_reads_by_size.py ${ribo_fa} ${ribo_size_fa} ${min_fplen} ${max_fplen}
+if [ ! -f ${rna_fa} ]; then
+    zcat ${rnaseq_fq} | ${fastx_pipe} -o ${rna_fa}
+    check_file ${rna_fa} "pipeline failed at preprocessing rnaseq_fq: ${rnaseq_fq}!"
+fi
+if [ ! -f ${ribo_fa} ]; then
+    zcat ${riboseq_fq} | ${fastx_pipe} | fastx_collapser -v -o ${ribo_fa}
+    check_file ${ribo_fa} "pipeline failed at preprocessing riboseq_fq: ${riboseq_fq}!"
+fi
+if [ ! -f ${ribo_size_fa} ]; then
+    python filter_reads_by_size.py ${ribo_fa} ${ribo_size_fa} ${min_fplen} ${max_fplen}
+    check_file ${ribo_size_fa} "pipeline failed at filtering riboseq with the wrong size!"
+fi
 #=============================
 # step 2: filter rrna
 #=============================
@@ -62,27 +83,21 @@ ornaprefix=${tmp_dir}${rna_core}_rrna_
 oriboprefix=${tmp_dir}${ribo_core}_rrna_
 rna_nrrna_fa=${ornaprefix}Unmapped.out.mate1
 ribo_nrrna_fa=${oriboprefix}Unmapped.out.mate1
-echo "filtering rrna"
+echo "filtering contaminated reads"
 if  [ ! -d ${rrna_idx} ];  then
-    echo "building rrna index..."
+    echo "building contaminant index..."
     mkdir -p ${rrna_idx}
     STAR --runThreadN $nproc --runMode genomeGenerate --genomeDir ${rrna_idx} --genomeFastaFiles ${rrna_fa} --genomeSAindexNbases 5 --genomeChrBinNbits 11
 fi
 if  [ ! -f ${rna_nrrna_fa} ];  then
-    echo "filtering rRNA in RNA_seq..."
+    echo "filtering contaminants in RNA_seq..."
     STAR --runThreadN $nproc --genomeDir ${rrna_idx} --readFilesIn ${rna_fa} --outFileNamePrefix ${ornaprefix} --outStd SAM --outReadsUnmapped Fastx --outSAMmode NoQS ${align_params} > /dev/null
-    if [ ! -f ${rna_nrrna_fa} ]; then
-        echo "pipeline failed at filtering rrna in RNA_seq"
-	exit
-    fi
+    check_file ${rna_nrrna_fa} "pipeline failed at filtering rrna in RNA_seq!"
 fi
 if  [ ! -f ${ribo_nrrna_fa} ];  then
-    echo "filtering rRNA in ribo_seq..."
+    echo "filtering contaminants in ribo_seq..."
     STAR --runThreadN $nproc --genomeDir ${rrna_idx} --readFilesIn ${ribo_fa} --outFileNamePrefix ${oriboprefix} --outStd SAM --outReadsUnmapped Fastx --outSAMmode NoQS ${align_params} > /dev/null
-    if [ ! -f ${ribo_nrrna_fa} ]; then
-        echo "pipeline failed at filtering rrna in ribo_seq"
-	exit
-    fi
+    check_file ${ribo_nrrna_fa} "pipeline failed at filtering rrna in ribo_seq!"
 fi
 #========================================
 # step 3: map to transcriptome
@@ -100,33 +115,28 @@ fi
 if [ ! -f ${rna_bam} ]; then
     echo "aligning RNA_seq to transcriptome..."
     STAR --runThreadN $nproc --genomeDir ${transcript_idx} --readFilesIn ${rna_nrrna_fa} --outFileNamePrefix ${ornaprefix} ${SAM_params} ${align_params}
-    if [ ! -f ${rna_bam} ]; then
-	echo "pipeline failed at mapping RNA_seq to transcriptome"
-	exit
-    fi
+    check_file ${rna_bam} "pipeline failed at mapping RNA_seq to transcriptome!"
 fi
 if [ ! -f ${ribo_bam} ]; then
     echo "aligning ribo_seq to transcriptome..."
     STAR --runThreadN $nproc --genomeDir ${transcript_idx} --readFilesIn ${ribo_nrrna_fa} --outFileNamePrefix ${oriboprefix} ${SAM_params} ${align_params}
-    if [ ! -f ${ribo_bam} ]; then
-	echo "pipeline failed at mapping ribo_seq to transcriptome"
-	exit
-    fi
+    check_file ${ribo_bam} "pipeline failed at mapping ribo_seq to transcriptome!"
 fi
 #============================================
 # step 4: salmon expression quantification
 #============================================
 sm_out=${sm_odir}/quant.sf
-if [ ! -f "${sm_out}" ]; then
+if [ ! -f ${sm_out} ]; then
     echo "running salmon quant..."
     salmon quant -t ${transcript_fa} -l U -a ${rna_bam} -o ${sm_odir} -p $nproc --bias_correct
-    if [ ! -f ${sm_out} ]; then
-	echo "pipeline failed at expression quantification"
-	exit
-    fi
+    check_file ${sm_out} "pipeline failed at expression quantification!"
 fi
 #=============================
 # step 5: run ribomap
 #=============================
 ribomap_out=${output_dir}/${ribo_core}_norm.profile
-echo "./ribomap --mrnabam ${rna_bam} --ribobam ${ribo_bam} --fasta ${transcript_fa} --sm ${sm_out} --out ${ribomap_out} --offset ${offset}"
+if [ ! -f ${ribomap_out} ]; then
+    echo "running riboprof..."
+    ./riboprof --mrnabam ${rna_bam} --ribobam ${ribo_bam} --fasta ${transcript_fa} --sf ${sm_out} --out ${ribomap_out} --offset ${offset}
+    check_file ${ribomap_out} "pipeline failed at ribosome profile generation!"
+fi
