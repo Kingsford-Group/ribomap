7665e5611d623e08abf638798f844a1aebff3921
Hao Wang <hw1@cs.cmu.edu>
2015-01-03 01:37:54 -0500

    revised python code to output gencode cds range file, fixed hela pipeline

--- scripts/get_hela_data.sh
+++ scripts/get_hela_data.sh
@@ -1,20 +1,39 @@
 #!/bin/bash
-cd ../data/
+work_dir=/home/hw1/scratch/ribomap/
+fasta_dir=${work_dir}data/fasta
+ref_dir=${work_dir}ref/human/
+#=============================
+# make directories
+#=============================
+mkdir -p ${fasta_dir}
+mkdir -p ${ref_dir}
+#=============================
+# step 1: download sra & refs
+#=============================
 echo "downloading data..."
-wget ftp://ftp.sanger.ac.uk/pub/gencode/Gencode_human/release_18/gencode.v18.pc_transcripts.fa.gz
-wget ftp://ftp.sanger.ac.uk/pub/gencode/Gencode_human/release_18/gencode.v18.annotation.gtf.gz
-wget ftp://ftp.sanger.ac.uk/pub/gencode/Gencode_human/release_18/gencode.v18.pc_translations.fa.gz
-wget ftp://ftp.ncbi.nlm.nih.gov/geo/samples/GSM546nnn/GSM546920/suppl/GSM546920_filtered_sequence.txt.gz
-wget ftp://ftp.ncbi.nlm.nih.gov/geo/samples/GSM546nnn/GSM546921/suppl/GSM546921_filtered_sequence.txt.gz
+cd ${ref_dir}
+wget ftp://ftp.sanger.ac.uk/pub/gencode/Gencode_human/release_21/gencode.v21.annotation.gtf.gz
+wget ftp://ftp.sanger.ac.uk/pub/gencode/Gencode_human/release_21/gencode.v21.pc_transcripts.fa.gz
+wget ftp://ftp.sanger.ac.uk/pub/gencode/Gencode_human/release_21/gencode.v21.pc_translations.fa.gz
 echo "unzipping data..."
 gunzip *.gz
-for file in *.txt; do
-    mv ${file} ${file%.txt}.fq
-done
+cd ${fasta_dir}
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/samples/GSM546nnn/GSM546920/suppl/GSM546920_filtered_sequence.txt.gz
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/samples/GSM546nnn/GSM546921/suppl/GSM546921_filtered_sequence.txt.gz
+#=============================
+# step 2: process reference
+# build cds range file
+#=============================
 echo "filtering transcripts...."
-python ../src/transcript_filter.py
-echo "getting rid of comment lines in gtf..."
-mv gencode.v18.annotation.gtf gencode.v18.annotation.gtf.bak
-sed '/^#/ d' < gencode.v18.annotation.gtf.bak > gencode.v18.annotation.gtf
-rm gencode.v18.annotation.gtf.bak
-echo "done."
+gtf=${ref_dir}gencode.v21.annotation.gtf
+tfa=${ref_dir}gencode.v21.pc_transcripts.fa
+pfa=${ref_dir}gencode.v21.pc_translations.fa
+python filter_gencode_transcript.py ${gtf} ${tfa} ${pfa}
+# echo "getting rid of comment lines in gtf..."
+# mv ${gtf} ${gtf}.bak
+# sed '/^#/ d' < ${gtf}.bak > ${gtf}
+# rm ${gtf}.bak
+#=============================
+# step 3: run ribomap
+#=============================
+
--- scripts/get_mouse_data.sh
+++ scripts/get_mouse_data.sh
@@ -1,28 +1,37 @@
 #!/bin/bash
-data_dir=/home/hw1/scratch/ribomap/data/
-sra_dir="${data_dir}"sra
-fasta_dir="${data_dir}"fasta
+work_dir=/home/hw1/scratch/ribomap/
+data_dir=${work_dir}data/
+sra_dir=${data_dir}sra
+fasta_dir=${data_dir}fasta
+ref_dir=${work_dir}ref/mouse/
 #=============================
 # make directories
 #=============================
 mkdir -p ${data_dir}
 mkdir -p ${sra_dir}
 mkdir -p ${fasta_dir}
+mkdir -p ${ref_dir}
 #=============================
-# step 1: download sra
+# step 1: download sra & refs
 #=============================
-cd ${sra_dir}
-echo "downloading data..."
-# ES cell feeder-free, w/ LIF 60 s CYH (100 ug/ml) mrna_mesc_yeslif Illumina GAII
-wget ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByExp/sra/SRX%2FSRX084%2FSRX084812/SRR315595/SRR315595.sra
-wget ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByExp/sra/SRX%2FSRX084%2FSRX084812/SRR315596/SRR315596.sra
-# ES cell feeder-free, w/ LIF 60 s CYH (100 ug/ml) ribo_mesc_chx Illumina GAII
-wget ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByExp/sra/SRX%2FSRX084%2FSRX084815/SRR315601/SRR315601.sra
-wget ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByExp/sra/SRX%2FSRX084%2FSRX084815/SRR315602/SRR315602.sra
-# ES cell feeder-free, w/ LIF 60 s CYH (100 ug/ml) ribo_mesc_yeslif Illumina GAII
-wget ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByExp/sra/SRX%2FSRX084%2FSRX084824/SRR315624/SRR315624.sra
-wget ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByExp/sra/SRX%2FSRX084%2FSRX084824/SRR315625/SRR315625.sra
-wget ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByExp/sra/SRX%2FSRX084%2FSRX084824/SRR315626/SRR315626.sra
+# echo "downloading data..."
+# cd ${sra_dir}
+# # ES cell feeder-free, w/ LIF 60 s CYH (100 ug/ml) mrna_mesc_yeslif Illumina GAII
+# wget ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByExp/sra/SRX%2FSRX084%2FSRX084812/SRR315595/SRR315595.sra
+# wget ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByExp/sra/SRX%2FSRX084%2FSRX084812/SRR315596/SRR315596.sra
+# # ES cell feeder-free, w/ LIF 60 s CYH (100 ug/ml) ribo_mesc_chx Illumina GAII
+# wget ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByExp/sra/SRX%2FSRX084%2FSRX084815/SRR315601/SRR315601.sra
+# wget ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByExp/sra/SRX%2FSRX084%2FSRX084815/SRR315602/SRR315602.sra
+# # ES cell feeder-free, w/ LIF 60 s CYH (100 ug/ml) ribo_mesc_yeslif Illumina GAII
+# wget ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByExp/sra/SRX%2FSRX084%2FSRX084824/SRR315624/SRR315624.sra
+# wget ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByExp/sra/SRX%2FSRX084%2FSRX084824/SRR315625/SRR315625.sra
+# wget ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByExp/sra/SRX%2FSRX084%2FSRX084824/SRR315626/SRR315626.sra
+cd ${ref_dir}
+wget ftp://ftp.sanger.ac.uk/pub/gencode/Gencode_mouse/release_M4/gencode.vM4.annotation.gtf.gz
+wget ftp://ftp.sanger.ac.uk/pub/gencode/Gencode_mouse/release_M4/gencode.vM4.pc_transcripts.fa.gz
+wget ftp://ftp.sanger.ac.uk/pub/gencode/Gencode_mouse/release_M4/gencode.vM4.pc_translations.fa.gz
+gunzip *.gz
+exit
 #==============================
 # step 2: convert sra to fastq
 #==============================
