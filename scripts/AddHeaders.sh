5f1eb37781b83531ac010d8ffb8b23fced72aa22
Hao Wang <hw1@cs.cmu.edu>
2015-02-10 00:30:46 -0500

    added license header script

---
+++ scripts/AddHeaders.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+usage()
+{
+cat << EOF
+usage: $0 options
+
+This script adds a header to all files matching the provided pattern in the given directory
+
+OPTIONS:
+   -h      Show this message
+   -l      license file 
+   -d      directory to search
+   -p      file pattern to match
+   -c      comment line prefix
+EOF
+}
+
+license=
+pattern=
+directory=
+char=
+while getopts "hl:p:d:c:" OPTION
+do
+     case $OPTION in
+         h)
+             usage
+             exit 1
+             ;;
+         l)
+             license=$OPTARG
+             ;;
+         p)
+             pattern=$OPTARG
+             ;;
+         d)
+             directory=$OPTARG
+             ;;
+	 c)
+	     char=$OPTARG
+	     ;;
+         ?)
+             usage
+             exit
+             ;;
+     esac
+done
+if [ ! -z "$char" ]; then
+    sed -i "s/^/$char/" $license
+fi
+
+echo "Prepending ${license} to files with pattern ${pattern} in directory ${directory}"
+
+
+for i in ${directory}/${pattern}
+do
+  if ! grep -q Copyright $i
+  then
+    cat ${license} $i >$i.new && mv $i $i.bak && mv $i.new $i
+  fi
+done
+
+if [ ! -z "$char" ]; then
+    sed -i "s/^$char//" $license
+fi
