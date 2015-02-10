2441b9266697740a051fb0430a0ce82efb670352
Hao Wang <hw1@cs.cmu.edu>
2015-02-10 14:44:22 -0500

    fixing include_prerequisites

--- scripts/include_prerequisites.sh
+++ scripts/include_prerequisites.sh
@@ -9,7 +9,7 @@ mkdir -p ${lib_dir}
 echo "downloading Saifish..."
 salmon_url=https://github.com/kingsfordgroup/sailfish/releases/download/v0.2.3/SalmonBeta-v0.2.3_ubuntu-14.04.tar.gz
 salmon_tarball=${pkg_dir}${salmon_url##*/}
-wget -P ${pkg_dir} -N https://github.com/kingsfordgroup/sailfish/releases/download/v0.2.3/SalmonBeta-v0.2.3_ubuntu-14.04.tar.gz
+wget -P ${pkg_dir} -N --no-check-certificate ${salmon_url}
 tar -zxvf ${salmon_tarball} -C ${pkg_dir}
 echo "copying Sailfish executables and libraries to bin/ lib/"
 salmon_dir=${salmon_tarball%.tar.gz}
@@ -18,7 +18,7 @@ cp ${salmon_dir}/lib/* ${lib_dir}
 echo "downloading Star..."
 star_url=https://github.com/alexdobin/STAR/archive/STAR_2.4.0h1.tar.gz
 star_tarball=${pkg_dir}${star_url##*/}
-wget -P ${pkg_dir} -N ${star_url}
+wget -P ${pkg_dir} -N --no-check-certificate ${star_url}
 tar -zxvf ${star_tarball} -C ${pkg_dir}
 echo "copying star executable to bin/"
 star_dir=${pkg_dir}STAR-STAR_2.4.0h1
